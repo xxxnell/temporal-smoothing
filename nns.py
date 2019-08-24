9d1bd0fe4d49af99c44e353df9db269b19348153
XXXNELL <xxxxxnell@gmail.com>
2019-08-24 20:35:50 +0900

    Implement ANN

---
+++ nns.py
@@ -0,0 +1,134 @@
+import math
+import numpy as np
+import tensorflow as tf
+from collections import Counter
+
+
+class ANN:
+    """
+    Approximate nearest neighbor search for rank 2 vector (i.e., x0 ⊕ x1 ⊕ ...) using LSH.
+    """
+
+    def __init__(self, lshs, i=None, xhashs=None, hashxs=None):
+        """
+        :param lshs: set of LSHs
+        :param i: index of x (i.e., x_i)
+        :param xhashs: [(x, (hash1, hash2, ...))]
+        :param hashxs: ({hash1: [x], ...}, {hash2: [x], ...})
+        """
+        if xhashs is None:
+            xhashs = []
+        if hashxs is None:
+            hashxs = tuple([{} for _ in range(len(lshs))])
+
+        self.lshs = lshs
+        self.xhashs = xhashs
+        self.hashxs = hashxs
+        self.i = i
+
+    @classmethod
+    def every(cls, dims, hash_no, w=None):
+        if w is None:
+            w = [0.5] * hash_no
+
+        lshs = []
+        for j in range(hash_no):
+            lshs.append(LSH(dims, w[j]))
+
+        anns = [cls(lshs)]
+        for i in range(len(dims)):
+            anns.append(cls(lshs, i))
+
+        return anns
+
+    def search(self, x):
+        hashs = tuple([lsh.hash(x, self.i) for lsh in self.lshs])
+        candidates = []
+        for hashx, hash in zip(self.hashxs, hashs):
+            if hash in hashx:
+                candidates += hashx[hash]
+        if self.i is None:
+            candidates = [tuple(candidate) for candidate in candidates]
+
+        count = Counter(candidates).most_common()
+        candidates = [candidate for candidate, n in count if n >= count[0][1]]
+
+        if len(candidates) <= 1:
+            nn = next(iter(candidates), None)
+        else:
+            nn = self.full_search(x, candidates)
+        if self.i is None and nn is not None:
+            nn = list(nn)
+
+        return nn
+
+    @staticmethod
+    def full_search(x, cs):
+        distances = []
+        for c in cs:
+            distances.append(tf.math.sqrt(sum([tf.math.square(tf.norm(c_i - x_i)) for c_i, x_i in zip(c, x)])))
+        return cs[tf.math.argmin(distances)]
+
+    def add(self, x):
+        hashs = tuple([lsh.hash(x, self.i) for lsh in self.lshs])
+
+        self.xhashs.append((x, hashs))
+
+        for hash, hashx_dict in zip(hashs, self.hashxs):
+            if hash in hashx_dict:
+                hashx_dict[hash].append(x)
+            else:
+                hashx_dict[hash] = [x]
+
+    def remove(self, x):
+        _hashs = ()
+        xhashs = []
+        for _x, hashs in self.xhashs:
+            if _x == x:
+                _hashs = hashs
+            else:
+                xhashs.append((_x, hashs))
+        self.xhashs = xhashs
+
+        for _hash, hashx in zip(_hashs, self.hashxs):
+            hashx[_hash].remove(x)
+            if len(hashx[_hash]) is 0:
+                hashx.pop(_hash, None)
+
+
+class LSH:
+    """
+    Stable distribution Locality-sensitive hashing (LSH) for rank 2 vector.
+    h(x) = ⌊ (a * x + b) / w ⌋, where a ~ N(0, 1) and b ~ U[0, w] for given w.
+
+    See https://en.wikipedia.org/wiki/Locality-sensitive_hashing#Stable_distributions or
+        http://mlwiki.org/index.php/Euclidean_LSH
+    """
+
+    def __init__(self, dims, w, cache_no=None):
+        if cache_no is None:
+            cache_no = [0] * len(dims)
+
+        a = [self._normal(dim) for dim in dims]
+        b = self._uniform(w)
+
+        self.w = w
+        self.a = a
+        self.b = b
+        self.dims = dims
+        self.cache_no = cache_no
+
+    @staticmethod
+    def _normal(dim):
+        return tf.random.normal(shape=[dim])
+
+    @staticmethod
+    def _uniform(w):
+        return np.random.uniform(high=w)
+
+    def hash(self, x, i=None):
+        if i is None:
+            ax = sum([tf.tensordot(a_i, x_i, 1) for a_i, x_i in zip(self.a, x)])
+        else:
+            ax = tf.tensordot(self.a[i], x, 1)
+        return math.ceil((ax + self.b) / self.w)
---
+++ tests/nns_test.py
@@ -0,0 +1,128 @@
+import unittest
+import tensorflow as tf
+from nns import LSH, ANN
+
+
+class TestANN(unittest.TestCase):
+
+    def test_every(self):
+        dims, hash_no = [1, 2, 3], 3
+        anns = ANN.every(dims, hash_no)
+        lshs = [ann.lshs for ann in anns]
+
+        self.assertEqual(len(anns), len(dims) + 1)
+        self.assertTrue(all([lsh1 is lsh2 for lsh1, lsh2 in zip(lshs[1:], lshs[:-1])]))
+        self.assertEqual([ann.i for ann in anns], [None] + list(range(0, len(dims))))
+
+    def test_search_0(self):
+        dims, hash_no = [1, 2, 3], 5
+        anns = ANN.every(dims, hash_no)
+        sumvec0 = [tf.ones(shape=[dim]) * 0.00 for dim in dims]
+        vecs0 = [sumvec0] + sumvec0
+
+        self.assertEqual([ann.search(vec0) for ann, vec0 in zip(anns, vecs0)], [None] * (len(dims) + 1))
+
+    def test_search_1(self):
+        dims, hash_no = [1, 2, 3], 5
+        anns = ANN.every(dims, hash_no)
+        sumvec0 = [tf.ones(shape=[dim]) * 0.00 for dim in dims]
+        sumvec1 = [tf.ones(shape=[dim]) * 0.01 for dim in dims]
+        vecs0 = [sumvec0] + sumvec0
+        vecs1 = [sumvec1] + sumvec1
+
+        for ann, vec1 in zip(anns, vecs1):
+            ann.add(vec1)
+
+        self.assertEqual([ann.search(vec0) for ann, vec0 in zip(anns, vecs0)], vecs1)
+
+    def test_search_2(self):
+        dims, hash_no = [1, 2, 3], 5
+        anns = ANN.every(dims, hash_no)
+        sumvec0 = [tf.ones(shape=[dim]) * 0.00 for dim in dims]
+        sumvec1 = [tf.ones(shape=[dim]) * 0.01 for dim in dims]
+        sumvec2 = [tf.ones(shape=[dim]) * 0.02 for dim in dims]
+        vecs0 = [sumvec0] + sumvec0
+        vecs1 = [sumvec1] + sumvec1
+        vecs2 = [sumvec2] + sumvec2
+
+        for ann, vec1, vec2 in zip(anns, vecs1, vecs2):
+            ann.add(vec1)
+            ann.add(vec2)
+
+        self.assertEqual([ann.search(vec0) for ann, vec0 in zip(anns, vecs0)], vecs1)
+
+    def test_full_search(self):
+        dims = [1, 2, 3]
+        sumvec0 = [tf.ones(shape=[dim]) * 0.00 for dim in dims]
+        sumvec1 = [tf.ones(shape=[dim]) * 0.01 for dim in dims]
+        sumvec2 = [tf.ones(shape=[dim]) * 0.02 for dim in dims]
+
+        self.assertEqual(ANN.full_search(sumvec0, [sumvec1, sumvec2]), sumvec1)
+
+    def test_add(self):
+        dims, hash_no = [1, 2, 3], 5
+        anns = ANN.every(dims, hash_no)
+        sumvec = [tf.random.normal(shape=[dim]) for dim in dims]
+        vecs = [tf.random.normal(shape=[dim]) for dim in dims]
+
+        anns[0].add(sumvec)
+        for ann, vec in zip(anns[1:], vecs):
+            ann.add(vec)
+
+        self.assertEqual([len(ann.xhashs) for ann in anns], [1] * (len(dims) + 1))
+        self.assertEqual([[len(xhashs[1]) for xhashs in ann.xhashs] for ann in anns], [[hash_no]] * (len(dims) + 1))
+
+        self.assertEqual([len(ann.hashxs) for ann in anns], [hash_no] * (len(dims) + 1))
+        self.assertEqual(
+            [[[len(value) for _, value in hashxs.items()] for hashxs in ann.hashxs] for ann in anns],
+            [[[1]] * hash_no] * (len(dims) + 1))
+
+    def test_remove(self):
+        dims, hash_no = [1, 2, 3], 5
+        anns = ANN.every(dims, hash_no)
+        sumvec = [tf.random.normal(shape=[dim]) for dim in dims]
+        vecs = [tf.random.normal(shape=[dim]) for dim in dims]
+
+        anns[0].add(sumvec)
+        anns[0].remove(sumvec)
+        for ann, vec in zip(anns[1:], vecs):
+            ann.add(vec)
+            ann.remove(vec)
+
+        self.assertEqual([ann.xhashs for ann in anns], [[]] * (len(dims) + 1))
+        self.assertEqual([ann.hashxs for ann in anns], [tuple([{}] * hash_no)] * (len(dims) + 1))
+
+
+class TestLSH(unittest.TestCase):
+
+    @unittest.skip
+    def test_op(self):
+        dims = [1, 2, 3]
+        lsh = LSH(dims=dims, w=3)
+        for _ in range(10):
+            x = [tf.random.normal(shape=[dim]) for dim in dims]
+            print("input: ", [str(x_i.numpy()) for x_i in x], ", hash: ", lsh.hash(x))
+
+    def test_init_1(self):
+        dims, w = [1, 2, 3], 10
+        lsh = LSH(dims=dims, w=w)
+        self.assertEqual([tf.shape(a_i).numpy() for a_i in lsh.a], [[dim] for dim in dims])
+        self.assertTrue(0 <= lsh.b <= w)
+
+    def test_init_2(self):
+        dims, w = [1, 2, 3], 10
+        lsh1 = LSH(dims=dims, w=w)
+        lsh2 = LSH(dims=dims, w=w)
+
+        self.assertNotEqual(lsh1.a, lsh2.a)
+        self.assertNotEqual(lsh1.b, lsh2.b)
+
+    def test_hash(self):
+        dims = [1, 2, 3]
+        lsh = LSH(dims=dims, w=10)
+        x = [tf.random.normal(shape=[dim]) for dim in dims]
+        self.assertIsInstance(lsh.hash(x), int)
+
+
+if __name__ == '__main__':
+    unittest.main()
